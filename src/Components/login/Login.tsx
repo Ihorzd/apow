3ff93577071df736b53f213c621a650dba67439c
Ihorzd <igorzdebskij@gmail.com>
2022-08-03 17:01:12 +0300

    add login page

--- src/Components/login/Login.tsx
+++ src/Components/login/Login.tsx
@@ -1,18 +1,23 @@
 import { FC } from "react";
 import './login.scss'
 
-const Login:FC = () => {
-    return(
+const Login: FC = () => {
+    return (
         <div className="login">
             <div className="loginImgContainer">
                 <h3>PLAYGROUND</h3>
-                <img src="/img/login/image1.png"/>
+                <img src="/img/login/image1.png" />
             </div>
             <div className="loginTxtContainer">
-                <h4>Login</h4>
-                <input />
-                <button>SEND CODE</button>
+                <div className="loginTxtContainer__content">
+                    <h4>Login</h4>
+                    <p>Enter your email </p>
+                    <input />
+                    <button>SEND CODE</button>
+                </div>
+
             </div>
+            <p className="copyrigtht">&copy; 2022 Playground. All rights reserved.</p>
         </div>
     )
 }
